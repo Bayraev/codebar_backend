092058aa741a54b00bfe01c7eb8c8ab6ed795c1c
Alialisk <99403653+Aliasik@users.noreply.github.com>
2023-06-10 14:13:09 +0300

    Added foreign key to snippet model. Need to get users Own data.

--- models/snippetModel.js
+++ models/snippetModel.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const snippetSchema = mongoose.Schema({
+  ownerId: { type: mongoose.Schema.Types.ObjectId, required: true }, // Belongs to [id]
   snippet: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
+  hidden: { type: Boolean, default: false }, // Able to another people to see it
   tags: Array,
   image: Array,
 });
